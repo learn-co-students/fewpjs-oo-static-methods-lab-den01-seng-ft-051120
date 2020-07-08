bbc7d6052c44f46222e24146d625637de074dc26
Carlos Fernandez <jcfernan@live.com>
2020-07-07 18:19:06 -0600

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-' ]+/g);
+    return string.replace(/[^A-Za-z0-9-' ]/g, "");
   }
+
+  static titleize(sentence) {
+    
+    var _titleizeWord = function(string) {
+            return string.charAt(0).toUpperCase() + 
+            string.slice(1).toLowerCase();
+        },
+        result = [];
+    sentence.split(" ").forEach(function(w) {
+        result.push(_titleizeWord(w));
+    });
+    return result.join(" ");
+}
 }
